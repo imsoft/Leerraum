66909f3b6c285dd383f9a6fefe6af41ca399860e
Brandon Uriel Garcia Ramos <brangarciaramos@gmail.com>
2026-02-23 14:47:12 -0600

    Initial Commit

---
+++ Leerraum/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Leerraum
+//
+//  Created by Brandon Uriel Garcia Ramos on 23/02/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Leerraum/LeerraumApp.swift
@@ -0,0 +1,17 @@
+//
+//  LeerraumApp.swift
+//  Leerraum
+//
+//  Created by Brandon Uriel Garcia Ramos on 23/02/26.
+//
+
+import SwiftUI
+
+@main
+struct LeerraumApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
